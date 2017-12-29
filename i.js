f920b7d5826ec773fc6f5442168067a9d5f62358
Rich <richardpaul25s@gmail.com>
2017-12-29 15:14:44 -0500

    OSWLA SOUNDS

---
+++ i.js
@@ -0,0 +1,40 @@
+{
+  "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+  "Awards": "Won 1 Oscar. Another 43 wins & 143 nominations.",
+  "BoxOffice": "$158,737,441",
+  "Country": "USA, UK, Canada, Iceland",
+  "DVD": "31 Mar 2015",
+  "Director": "Christopher Nolan",
+  "Genre": "Adventure, Drama, Sci-Fi",
+  "Language": "English",
+  "Metascore": "74",
+  "Plot": "Earth's future has been riddled by disasters, famines, and droughts. There is only one way to ensure mankind's survival: Interstellar travel. A newly discovered wormhole in the far reaches of our solar system allows a team of astronauts to go where no man has gone before, a planet that may have the right environment to sustain human life.",
+  "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+  "Production": "Paramount Pictures",
+  "Rated": "PG-13",
+  "Ratings": [
+    {
+      "Source": "Internet Movie Database",
+      "Value": "8.6/10"
+    },
+    {
+      "Source": "Rotten Tomatoes",
+      "Value": "71%"
+    },
+    {
+      "Source": "Metacritic",
+      "Value": "74/100"
+    }
+  ],
+  "Released": "07 Nov 2014",
+  "Response": "True",
+  "Runtime": "169 min",
+  "Title": "Interstellar",
+  "Type": "movie",
+  "Website": "http://www.InterstellarMovie.com/",
+  "Writer": "Jonathan Nolan, Christopher Nolan",
+  "Year": "2014",
+  "imdbID": "tt0816692",
+  "imdbRating": "8.6",
+  "imdbVotes": "1,112,625"
+}
